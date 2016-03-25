48f0d5daf84075a71cc7fa2d82d5a50b7f505a76
Thomas Smits <thomsmits@gmail.com>
2016-03-25 13:14:02 +0100

    Added jar sync

--- publish_javadoc.sh
+++ publish_javadoc.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Publish the JavaDoc to GitHub pages 
 GIT_REPO=`pwd`
 pushd ~/Temp/
 rm -rf game-framework 
@@ -8,7 +9,11 @@ git checkout gh-pages
 rsync -raz $GIT_REPO/javadoc/ javadoc/
 git add javadoc
 git commit -m "Updated JavaDoc"
-git push origin master
+cp $GIT_REPO/out/artifacts/game_framework_jar/game_framework.jar .
+git add game_framework.jar
+git commit -m "Updated binaries"
+git push origin gh-pages
 cd ..
 rm -rf game-framework
 popd
+
